f6256b0ace6bd7d3b14ecbe2e6f4832658c171ac
Macitron <117962989+Macitron@users.noreply.github.com>
2025-12-02 00:21:30 -0800

    Day 1 part 2 solved
    - that was way too hard for me

--- 01/rust/src/main.rs
+++ 01/rust/src/main.rs
@@ -1,26 +1,46 @@
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let mut infile = BufReader::new(File::open("../input").unwrap());
-
-    let part1_answer = part1(&mut infile);
+    let part1_answer = solve(&mut infile, &Part::One);
     println!("Part 1: password = {part1_answer}");
+
+    infile = BufReader::new(File::open("../input").unwrap());
+    let part2_answer = solve(&mut infile, &Part::Two);
+    println!("Part 2: password = {part2_answer}");
 }
 
-fn part1(reader: &mut impl BufRead) -> u32 {
+enum Part {
+    One,
+    Two,
+}
+
+fn solve(reader: &mut impl BufRead, part: &Part) -> u32 {
     let mut zeros = 0;
-    let mut sum = 50;
+    let mut pos = 50;
 
     for line in reader.lines() {
         let line = line.unwrap();
         let (dir, count) = line.split_at(1);
-        let operand = count.parse::<i32>().unwrap() * if dir == "L" { -1 } else { 1 };
+        let add = count.parse::<i32>().unwrap() * if dir == "L" { -1 } else { 1 };
 
-        sum = (sum + operand + 100) % 100;
-        if sum == 0 {
-            zeros += 1;
-        }
+        zeros += match part {
+            Part::One => {
+                let new_pos = (pos + add).rem_euclid(100);
+                u32::from(new_pos == 0)
+            }
+            Part::Two => {
+                let raw_sum = pos + add;
+                match raw_sum.cmp(&0) {
+                    Ordering::Greater => (raw_sum / 100).unsigned_abs(),
+                    Ordering::Equal => 1,
+                    Ordering::Less => u32::from(pos != 0) + (raw_sum / 100).unsigned_abs(),
+                }
+            }
+        };
+        pos = (pos + add).rem_euclid(100);
     }
 
     zeros
