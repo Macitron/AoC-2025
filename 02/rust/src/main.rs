42459bb27ac2d302d9ce26db05e284720b4b850a
Macitron <117962989+Macitron@users.noreply.github.com>
2025-12-02 23:42:35 -0800

    Day 2 part 2

--- 02/rust/src/main.rs
+++ 02/rust/src/main.rs
@@ -21,6 +21,9 @@ fn main() {
 
     let part1_answer = part1(&ranges);
     println!("Part 1: Invalid ID sum = {part1_answer}");
+
+    let part2_answer = part2(&ranges);
+    println!("Part 2: Invalid ID sum = {part2_answer}");
 }
 
 fn part1(ranges: &[(u64, u64)]) -> u64 {
@@ -43,4 +46,49 @@ fn part1(ranges: &[(u64, u64)]) -> u64 {
 
     // what do you even call this
     kinda_palindrome_sum
+}
+
+fn part2(ranges: &[(u64, u64)]) -> u64 {
+    let mut invalid_id_sum = 0;
+    /* Ideas
+    start with the first char, look forward for that char, see if the string is a multiple of that
+    distance between them. if it is, start checking the match in tandem
+     */
+
+    for (start, end) in ranges {
+        for n in *start..=*end {
+            let n_chars = n.to_string().chars().collect::<Vec<char>>();
+            let first_char = n_chars.get(0).unwrap();
+
+            for sublen in 1..=n_chars.len() / 2 {
+                if n_chars.get(sublen).unwrap() == first_char
+                    && n_chars.len() % sublen == 0
+                    && check_substr(&n_chars, sublen)
+                {
+                    invalid_id_sum += n;
+                    break;
+                }
+            }
+        }
+    }
+
+    invalid_id_sum
+}
+
+// Invariants:
+// 1. `sublen` divides str.len()
+// 2. `str[sublen]` is equal to `str[0]`
+// so we're just checking those substrings
+fn check_substr(n_chars: &[char], sublen: usize) -> bool {
+    let first_substr = &n_chars[0..sublen];
+    let repeats = n_chars.len() / sublen;
+
+    for iter in 1..repeats {
+        let offset = iter * sublen;
+        if &n_chars[offset..offset + sublen] != first_substr {
+            return false;
+        }
+    }
+
+    true
 }
